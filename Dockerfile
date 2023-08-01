d2c51e10676fc1dc2cf6f4e91a18c81470b84572
Mau <mauricio.jimenez@hotmail.com>
2023-07-31 19:51:43 -0600

    Se agrega compilación maven

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:8u275-jdk-oracle
-WORKDIR /
-ADD target/ambiente-1.0.jar ambiente.jar
-CMD java -jar ambiente.jar 
-
--- src/main/java/mx/com/sye/pruebas/ambiente/util/Propiedades.java
+++ src/main/java/mx/com/sye/pruebas/ambiente/util/Propiedades.java
@@ -5,15 +5,27 @@ import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+/**
+ * Clase para obtener los valores de las variables de ambiente
+ * @author Mauricio Jiménez
+ * @version 1.0
+ * @since Agosto, 2022
+ */
 @Component
 public class Propiedades implements EnvironmentAware {
 
 	@Autowired
 	public static Environment environment;
 	
+    /**
+     * Método para obtener la variable de entorno
+     * @param key - nombre de la variable
+     * @return Valor de la variable de entorno
+     */
     public static  String getConfigProp(String key) {
         return environment.getProperty(key);
     }
+
     @Override
     public void setEnvironment(Environment arg0) {
         if(environment==null){
